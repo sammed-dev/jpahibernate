58156b6fc6c2c9b77fd6e29e461169752c43e84b
sammed-dev <sammed.sankonatti@datagrokr.com>
2022-04-04 16:43:46 +0530

    so the entitymanager alone has limited methods to perform with database

--- src/main/java/com/datagrokr/simplejpa/App.java
+++ src/main/java/com/datagrokr/simplejpa/App.java
@@ -1,8 +1,25 @@
 package com.datagrokr.simplejpa;
 
+import com.datagrokr.simplejpa.model.Student;
+
 public class App 
 {
     public static void main(String[] args) {
-        
+        Student student = new Student();
+        student.setFirstName("Ab");
+        student.setLastName("devilliars");
+
+        StudentRepository studentRepository = new StudentRepository();
+        // studentRepository.addStudent(student);
+        // System.out.println(student.toString());
+
+
+        student = studentRepository.findStudent(8L);
+        System.out.println(student);
+
+        student.setFirstName("Ab");
+        studentRepository.updateStudent(student);
+    
+        studentRepository.deleteStudent(student);
     }
 }
