047ed5efd6a1ff4f4e90da00af692e8fa6e35448
sammed-dev <sammed.sankonatti@datagrokr.com>
2022-04-04 15:06:11 +0530

    way to update the entity

---
+++ src/main/java/com/datagrokr/simplejpa/UpdateStudent.java
@@ -0,0 +1,24 @@
+package com.datagrokr.simplejpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import com.datagrokr.simplejpa.model.Student;
+
+public class UpdateStudent {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("student_pu");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+
+        entityTransaction.begin();
+        Student student = entityManager.find(Student.class, 3L);
+        student.setFirstName("virat");
+        student.setLastName("King kohli");
+        entityTransaction.commit();
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+}
