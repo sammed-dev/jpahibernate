91fb81411532fc203ed3595027a1838cf6f0e946
sammed-dev <sammed.sankonatti@datagrokr.com>
2022-04-04 14:42:22 +0530

    jpa

--- src/main/java/com/datagrokr/simplejpa/CreateStudent.java
+++ src/main/java/com/datagrokr/simplejpa/CreateStudent.java
@@ -6,9 +6,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.persistence.spi.PersistenceProvider;
-import javax.transaction.Transactional;
-
 public class CreateStudent {
 
     public static void main(String[] args) {
