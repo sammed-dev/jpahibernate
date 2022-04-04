095896dc46411ca2f60236a5647f4f3258fc3ea6
sammed-dev <sammed.sankonatti@datagrokr.com>
2022-04-04 14:39:17 +0530

    initial setup

---
+++ src/main/java/com/datagrokr/simplejpa/CreateStudent.java
@@ -0,0 +1,31 @@
+
+package com.datagrokr.simplejpa;
+
+import com.datagrokr.simplejpa.model.Student;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.spi.PersistenceProvider;
+import javax.transaction.Transactional;
+
+public class CreateStudent {
+
+    public static void main(String[] args) {
+
+//        PersistenceProvider provider = new HibernatePersistenceProvider();
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("student_pu");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+
+        entityTransaction.begin();
+        Student student = new Student();
+        student.setFirstName("shreyank");
+        student.setLastName("jain");
+        entityManager.persist(student);
+        entityTransaction.commit();
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+}
+
